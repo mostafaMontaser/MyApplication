34c938653e25bf694df5685014c2a9862bf54122
mostafa <mostafa.m.montaser@gmail.com>
2016-03-02 12:16:58 +0200

    Rejected commit: Default

--- app/src/main/java/demo/jets/iti/myapplication/FragA.java
+++ app/src/main/java/demo/jets/iti/myapplication/FragA.java
@@ -14,4 +14,9 @@ public class FragA extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fraga,container,false);
     }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
