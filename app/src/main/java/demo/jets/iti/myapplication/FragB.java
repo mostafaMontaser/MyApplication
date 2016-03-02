0f9cf95b4f268994122cbcd0b0e0aa35779cbaf3
mostafa <mostafa.m.montaser@gmail.com>
2016-03-02 15:00:31 +0200

    test

--- app/src/main/java/demo/jets/iti/myapplication/FragB.java
+++ app/src/main/java/demo/jets/iti/myapplication/FragB.java
@@ -2,6 +2,7 @@ package demo.jets.iti.myapplication;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,5 +20,10 @@ public class FragB extends Fragment{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
     }
 }
